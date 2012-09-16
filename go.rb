fbd3e53aec25aaf33ef0b992c468782cd5dae0a8
Jose Goncalves <zetoeu@gmail.com>
2012-09-16 01:17:36 +0100

    mean,median,mode for hacker news

--- go.rb
+++ go.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
 require 'open-uri'
 require 'json'
+require './lib/hacker_news'
 
 get '/' do
   @news = HackerNews.front_page_news
+  @points = @news.map {|n| n['points']} 
+  @mean = @points.mean
+  @median = @points.median
+  @mode = @points.mode
+
+  @news_above_median = @news.select { |n| n['points'] > @median}
+
+  erb :index
 end
