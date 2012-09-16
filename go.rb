c0bb1f0b180c067adbefce643670a8c74fe15829
Jose Goncalves <zetoeu@gmail.com>
2012-09-16 02:29:20 +0100

    refactor for keeping it DRY

--- go.rb
+++ go.rb
@@ -4,13 +4,7 @@ require 'json'
 require './lib/hacker_news'
 
 get '/' do
-  @news = HackerNews.front_page_news
-  @points = @news.map {|n| n['points']} 
-  @mean = @points.mean
-  @median = @points.median
-  @mode = @points.mode
-
-  @news_above_median = @news.select { |n| n['points'] > @median}
+  @hacker_news = HackerNews.new
 
   erb :index
 end
--- lib/hacker_news.rb
+++ lib/hacker_news.rb
@@ -2,12 +2,41 @@ require 'open-uri'
 require 'json'
 require './lib/array'
 
-module HackerNews
-  def self.front_page_news
-    hacker_news = JSON.parse(open('http://api.ihackernews.com/page').read) rescue 'Cant read Hacker News'
+class HackerNews
+
+  attr_accessor :news
+
+  def initialize
+    load
+  end
+
+  def load
+    news = JSON.parse(open('http://api.ihackernews.com/page').read) rescue 'Cant read Hacker News'
     
-    if hacker_news.has_key?('items') && hacker_news['items'].any?
-      hacker_news['items']
+    if news.has_key?('items') && news['items'].any?
+      @news = news['items']
     end
+  end
+
+  def mean
+    points.mean
+  end
+
+  def median
+    points.median
+  end
+
+  def mode
+    points.mode
+  end
+
+  def above_median
+    news.select { |n| n['points'] > median}
+  end
+
+  private
+
+  def points
+    news.map {|n| n['points']}
   end
 end
